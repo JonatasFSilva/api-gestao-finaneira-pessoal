74197f5bc6b984b51c750c866ae950153690f0c4
Jonatas Fabricio da Silva <62157990+JonatasFSilva@users.noreply.github.com>
2025-03-28 10:44:37 -0300

    initial commit

---
+++ src/main/java/com/api/gestao/financeira/pessoal/apigestao_financeira_pessoal/ApigestaoFinanceiraPessoalApplication.java
@@ -0,0 +1,13 @@
+package com.api.gestao.financeira.pessoal.apigestao_financeira_pessoal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApigestaoFinanceiraPessoalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApigestaoFinanceiraPessoalApplication.class, args);
+	}
+
+}
